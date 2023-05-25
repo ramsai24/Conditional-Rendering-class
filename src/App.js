ce84c380e30daff101664635425f69e4bd7c2f45
ramsai24 <jnrsai24@gmail.com>
2023-05-25 08:34:50 +0000

    element variable conditional rendering

--- src/App.js
+++ src/App.js
@@ -5,27 +5,42 @@ import Welcome from './components/Welcome'
 import './App.css'
 
 class App extends Component {
-  //   state = {
-  //     isLoggedIn: true,
-  //   }
   state = {
-    isLoggedIn: false,
+    isLoggedIn: true,
   }
+  //   state = {
+  //     isLoggedIn: false,
+  //   }
 
-  renderAuthButton = () => {
-    const {isLoggedIn} = this.state
+  // if - else condition rendering
 
-    if (isLoggedIn === true) {
-      return <button>Logout</button>
-    }
-    return <button>Login</button>
-  }
+  //   renderAuthButton = () => {
+  //     const {isLoggedIn} = this.state
+
+  //     if (isLoggedIn === true) {
+  //       return <button>Logout</button>
+  //     }
+  //     return <button>Login</button>
+  //   }
 
   render() {
+    const {isLoggedIn} = this.state
+    let authButton
+
+    if (isLoggedIn === true) {
+      authButton = <button>Logout</button>
+    } else {
+      authButton = <button>Login</button>
+    }
     return (
       <div className="container">
         <Welcome greeting="Hello" name="User" />
-        {this.renderAuthButton()}
+
+        {/* // if - else condition rendering */}
+        {/* {this.renderAuthButton()} */}
+
+        {/* if - else using Element Variables  */}
+        {authButton}
       </div>
     )
   }
